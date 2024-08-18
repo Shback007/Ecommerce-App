dac8913688a6a7d19d57a5c97c659c60c0783a4e
Sagnik Hore <sagnikhoredht@gmail.com>
2024-08-18 20:12:39 +0530

    Updated

--- frontend/src/components/Assets/all_product.js
+++ frontend/src/components/Assets/all_product.js
@@ -43,6 +43,10 @@ let all_product = [
     image: p1_img,
     new_price: 50.0,
     old_price: 80.5,
+    type: 'faux fur jacket',
+    type2: "women's outerwear",
+    description: `Elevate your winter style with this luxurious faux fur jacket, designed to keep you warm while looking effortlessly chic. This jacket features a plush, velvety texture with a rich, deep black hue that adds a touch of elegance to any outfit. The jacket is crafted with a high collar for extra warmth and a stylish, structured fit that flatters your silhouette. It’s perfect for both casual outings and more formal events, making it a versatile addition to your wardrobe. Pair it with jeans for a laid-back look or throw it over a dress for a night out. Embrace the colder months in style with this must-have faux fur jacket!`,
+    rated:198,
   },
   {
     id: 2,
@@ -51,6 +55,10 @@ let all_product = [
     image: p2_img,
     new_price: 85.0,
     old_price: 120.5,
+    type: 'ribbed knit t-shirt',
+    type2: "women's tops",
+    description: `Add a pop of color to your wardrobe with this chic pink ribbed knit t-shirt. Designed with a flattering square neckline, this top combines simplicity with elegance, making it perfect for both casual and semi-formal occasions. The soft, stretchy ribbed fabric ensures a comfortable fit that hugs your body in all the right places. The short sleeves and tailored silhouette offer a classic yet modern look, making it easy to pair with anything from jeans to skirts.`,
+    rated: 231,
   },
   {
     id: 3,
@@ -59,6 +67,10 @@ let all_product = [
     image: p3_img,
     new_price: 60.0,
     old_price: 100.5,
+    type: 'colorblock crop top',
+    type2: "women's tops",
+    description:`Make a bold statement with this eye-catching colorblock crop top. Featuring a unique design with contrasting shades of tan, rust, and white, this top is perfect for those who love to stand out. The sleeveless design and cropped fit make it ideal for warm weather or for layering under a jacket. Crafted from a soft, breathable fabric, this crop top ensures all-day comfort while keeping you on-trend. The front is accented with a small, stylish logo patch, adding a sporty touch to the overall look. Pair it with high-waisted jeans, shorts, or a skirt for a fashionable, effortless outfit. This crop top is a must-have for anyone looking to add some flair to their casual wardrobe!`,
+    rated: 302
   },
   {
     id: 4,
@@ -139,6 +151,10 @@ let all_product = [
     image: p13_img,
     new_price: 85.0,
     old_price: 120.5,
+    type: "hoodie",
+    type2:"sweatShirt",
+    description:`This hooded sweat jacket is designed for fans of Formula 1 racing and features the Puma Motorsport logo and the team's colors. It is made of a comfortable cotton blend and has a relaxed fit. The jacket features a full-zip closure, kangaroo pockets, and a drawstring hood.`,
+    rated:145,
   },
   {
     id: 14,
@@ -147,6 +163,10 @@ let all_product = [
     image: p14_img,
     new_price: 85.0,
     old_price: 120.5,
+    type:"jacket",
+    type2:"outwear",
+    description:`This men's color-blocked jacket is a stylish blend of white, dark blue, and red, offering a modern, sporty look. The jacket features a high collar and a zippered front, making it both practical and fashionable. The quilted design provides lightweight warmth, perfect for layering during cooler weather. The contrasting colors add a pop of visual interest, making it a versatile piece for casual outings or weekend adventures.`,
+    rated:207,
   },
   {
     id: 15,
@@ -155,6 +175,10 @@ let all_product = [
     image: p15_img,
     new_price: 85.0,
     old_price: 120.5,
+    type:"jackets",
+    type2:"Casual wear",
+    description:`The JACK & JONES Full Sleeve Printed Men's Jacket is a stylish and versatile piece for men. It features a full-sleeve design with a printed pattern, adding a touch of personality to any outfit. The jacket is likely made from a comfortable fabric blend suitable for various weather conditions. It offers a modern and casual look, perfect for both everyday wear and casual outings.`,
+    rated:109,
   },
   {
     id: 16,
@@ -163,6 +187,10 @@ let all_product = [
     image: p16_img,
     new_price: 85.0,
     old_price: 120.5,
+    type: "jackets",
+    type2: "waterproof jackets",
+    description:`The ACTVE Urban Street Waterproof Red Rain Jacket is a stylish and functional piece of outerwear designed for urban environments. It features a vibrant red color, likely made from a waterproof material to keep you dry in rainy weather. The jacket probably has a modern and contemporary design, suitable for everyday wear and outdoor activities.`,
+    rated:167,
   },
   {
     id: 17,
@@ -171,6 +199,11 @@ let all_product = [
     image: p17_img,
     new_price: 85.0,
     old_price: 120.5,
+    type:"jackets",
+    type2:"denim jackets",
+    description:`The American Eagle Outfitters Full Sleeve Washed Men's Jacket is a casual and comfortable piece of outerwear. It features a washed finish, giving it a relaxed and lived-in look. The jacket is likely made from a soft cotton blend and offers a comfortable fit. It's perfect for layering and can be dressed up or down.`,
+    rated:245,
+
   },
   {
     id: 18,
@@ -179,6 +212,11 @@ let all_product = [
     image: p18_img,
     new_price: 85.0,
     old_price: 120.5,
+    type:"casual jackets",
+    type2:"spring jackets",
+    description:`The Technosport Men's Full Sleeve Solid Men Jacket is a versatile and classic piece of outerwear. It features a solid color design, making it easy to pair with various outfits. The jacket is likely made from a comfortable fabric suitable for different weather conditions. It offers a clean and simple look, perfect for both casual and semi-formal occasions.`,
+    rated:190,
+
   },
   {
     id: 19,
@@ -187,6 +225,10 @@ let all_product = [
     image: p19_img,
     new_price: 85.0,
     old_price: 120.5,
+    type:"jackets",
+    type2:"sports jackets",
+    description:`The HRX by Hrithik Roshan Men's Full Sleeve Solid Sports Jacket is a performance-oriented jacket designed for athletic activities. It is likely made from moisture-wicking and breathable fabric to keep the wearer comfortable during workouts. The jacket likely features a modern and sporty design, incorporating elements like reflective details or ventilation zones. It is designed to offer a balance of style and functionality for active individuals.`,
+    rated:209,
   },
   {
     id: 20,
@@ -195,6 +237,10 @@ let all_product = [
     image: p20_img,
     new_price: 85.0,
     old_price: 120.5,
+    type:"sweatshirts",
+    type2:"oversized shirt",
+    description:`The Roadster Lifestyle Co. Men's Fleece Sweatshirt is a comfortable and casual piece of clothing perfect for everyday wear. Crafted from soft fleece fabric, it provides warmth and coziness. The sweatshirt likely features a classic pullover style with a ribbed neckline, cuffs, and hem. It might come in a variety of solid colors or trendy patterns to suit different preferences. This sweatshirt is designed to offer a relaxed fit and is suitable for layering with other garments.`,
+    rated:256,
   },
   {
     id: 21,
@@ -203,6 +249,10 @@ let all_product = [
     image: p21_img,
     new_price: 85.0,
     old_price: 120.5,
+    type:'jackets',
+    type2:'outerwear',
+    description:`The Zeel Men's Rain Jacket is a waterproof and windproof garment designed to protect the wearer from inclement weather conditions. It likely features a hood, zipper closure, and adjustable cuffs and hem for optimal protection. The jacket is probably made from a durable, water-resistant fabric and may include additional features like reflective details for visibility.`,
+    rated:108,
   },
   {
     id: 22,
@@ -211,6 +261,10 @@ let all_product = [
     image: p22_img,
     new_price: 85.0,
     old_price: 120.5,
+    type:'hoodies',
+    type2:'outerwear',
+    description:`The LOCOMOTIVE Men White Solid Puffer Jacket is a winter essential designed to keep you warm and stylish. It features a classic puffer jacket design with a solid white color. The jacket is likely filled with insulating material to provide warmth, and it may have features like a hood, zipper closure, and multiple pockets.`,
+    rated:236,
   },
   {
     id: 23,
@@ -219,6 +273,10 @@ let all_product = [
     image: p23_img,
     new_price: 85.0,
     old_price: 120.5,
+    type: 'jackets',
+    type2:'sporty hoodie',
+    description:`The hoodie has a relaxed fit, a drawstring hood, and a kangaroo pocket, providing both comfort and a pop of color to the outfit. The person is also wearing white pants, which complement the vibrant hoodie.`,
+    rated:45
   },
   {
     id: 24,
@@ -227,6 +285,10 @@ let all_product = [
     image: p24_img,
     new_price: 85.0,
     old_price: 120.5,
+    type:'jackets',
+    type2:'leather jackets',
+    description:`The jacket is made from high-quality leather and features a solid color (brown) throughout. It has a full-length zipper closure, multiple pockets, and a collar that can be zipped up for added protection from the elements.`,
+    rated:307,
   },
   {
     id: 25,
